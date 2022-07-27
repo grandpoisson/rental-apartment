4b53b737a4fdc1b44490a02e4bf70938da5a3f30
grandpoisson <grandpoisson@gmail.com>
2022-07-27 23:04:14 +0300

    send confirmation emails

--- pages/api/stripe/webhook.js
+++ pages/api/stripe/webhook.js
@@ -1,5 +1,6 @@
 import prisma from "lib/prisma";
 import getRawBody from "raw-body";
+import sendEmail from "lib/email";
 
 export const config = {
   api: {
@@ -60,6 +61,24 @@ export default async (req, res) => {
           sessionId,
         },
       });
+
+      sendEmail(
+        "you@youremail.com",
+        "New booking",
+        `${email} booked from ${new Date(
+          booking.from
+        ).toDateString()} to ${new Date(booking.to).toDateString()}`
+      );
+
+      sendEmail(
+        email,
+        "Thanks for booking",
+        `Your booking from ${new Date(
+          booking.from
+        ).toDateString()} to ${new Date(
+          booking.to
+        ).toDateString()} is confirmed!`
+      );
     } catch (err) {
       console.error(err);
     }
